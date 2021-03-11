1d64df9e0efa2f6c3ff0901e1b565a5c930fde2c
Greg Cobb <gcobb@pivotal.io>
2021-03-10 17:15:26 -0800

    Update to run on cloud foundry
    - Run on port 8080
    - Serve h2c instead of h2
    - Use go mod
    
    Authored-by: Greg Cobb <gcobb@pivotal.io>

--- main.go
+++ main.go
@@ -5,10 +5,13 @@ import (
 	"io"
 	"net/http"
 	"time"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 const (
-	address  = ":8443"
+	address  = ":8080"
 	PAGE_URL = "/"
 	PAGE     = `<!DOCTYPE html>
 <html>
@@ -33,7 +36,7 @@ func page(w http.ResponseWriter, r *http.Request) {
 		// HTTP/2 Push is supported
 		w.Header().Set("Link", "<script.js>; rel=preload; as=script")
 		if err := pusher.Push(SCRIPT_URL, nil); err != nil {
-			fmt.Printf("Failder to push: %v\n", err)
+			fmt.Printf("Failed to push: %v\n", err)
 		}
 	}
 	w.Header().Set("Content-Type", "text/html")
@@ -45,11 +48,12 @@ func main() {
 	http.HandleFunc(SCRIPT_URL, script)
 	server := &http.Server{
 		Addr:         address,
+		Handler:      h2c.NewHandler(http.DefaultServeMux, &http2.Server{}),
 		ReadTimeout:  10 * time.Second,
 		WriteTimeout: 10 * time.Second,
 	}
 	fmt.Printf("Listening on port %s\n", address)
-	err := server.ListenAndServeTLS("server.crt", "server.key")
+	err := server.ListenAndServe()
 	if err != nil {
 		fmt.Println(err)
 	}
